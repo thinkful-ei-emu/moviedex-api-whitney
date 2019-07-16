aa59bb4f53a46fc8c1a68d2776e4c95c6827d91b
Whitney Sam <whitneysamwallace@gmail.com>
2019-07-16 13:15:07 -0700

    authorization for API key functional

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
 app.use(morgan('common'));
-
+app.use(validateBearerToken);
 //Endpoint is GET /movie
 //Search options for genre, country, and/or average vote
 //Searching by genre -> whether movie's genre includes string, case insensitive
@@ -13,8 +14,24 @@ app.use(morgan('common'));
 
 //Endpoint only responds give a valid Authorization header with a Bearer API token value
 //Endpoint should have general security, CORS & Helmet?
+function validateBearerToken(req, res, next) {
+  const API_TOKEN = process.env.API_TOKEN;
+  const authVal = req.get('Authorization') || '';
+  const [ authType, token ] = authVal.split(' ');
+  if(authType.toLowerCase() !== 'bearer') {
+    return res.status(401).json({ error: 'Not authorized to make that request'});
+  }
+  if(token !== API_TOKEN) {
+    return res.status(401).json({ error: 'Not authorized to make that request'});
+  }
 
-app.get('/movie', (req, res) => {
-});
+  next();
+}
+const validSearch = ['genre', 'country', 'avg_vote'];
+
+function handleGetSearch(req, res) {
+  res.json(validSearch);
+}
+app.get('/movie', handleGetSearch);
 
 app.listen(8000, () => console.log('Server Started 8000'));
