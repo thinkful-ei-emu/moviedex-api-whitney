5d84ad2132669aa9dc0a361610f3b6da652d4faa
Whitney Sam <whitneysamwallace@gmail.com>
2019-07-16 13:51:27 -0700

    response to 3 search parameters functional

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const movies = require('./moviedata.json');
 
 const app = express();
 app.use(morgan('common'));
 app.use(validateBearerToken);
-//Endpoint is GET /movie
-//Search options for genre, country, and/or average vote
-//Searching by genre -> whether movie's genre includes string, case insensitive
-//Searching by country -> whether the movie's country includes a specific string, case insensitive
-//Searching by average vote -> whether movie's avg_vote is >= # supplied
-//Api responds with array of full movie entries for search results
 
-//Endpoint only responds give a valid Authorization header with a Bearer API token value
 //Endpoint should have general security, CORS & Helmet?
 function validateBearerToken(req, res, next) {
   const API_TOKEN = process.env.API_TOKEN;
@@ -27,11 +21,29 @@ function validateBearerToken(req, res, next) {
 
   next();
 }
-const validSearch = ['genre', 'country', 'avg_vote'];
 
-function handleGetSearch(req, res) {
-  res.json(validSearch);
+function handleGetMovie (req, res) {
+  //Search options for genre, country, and/or avg_vote
+  let results = movies;
+
+  //Searching by genre -> whether movie's genre includes string, case insensitive
+  if(req.query.genre) {
+    results = results.filter(result => result.genre.toLowerCase().includes(req.query.genre.toLowerCase()));
+  }
+  //Searching by country -> whether the movie's country includes a specific string, case insensitive
+  if(req.query.country) {
+    results = results.filter(result => result.country.toLowerCase().includes(req.query.country.toLowerCase()));
+  }
+  //Searching by average vote -> whether movie's avg_vote is >= # supplied
+  if(req.query.avg_vote) {
+    results = results.filter(result => result.avg_vote >= req.query.avg_vote);
+  }
+  //Api responds with array of full movie entries for search results
+  return res.json(results);
+
 }
-app.get('/movie', handleGetSearch);
+//Endpoint is GET /movie
+app.get('/movie', handleGetMovie);
 
-app.listen(8000, () => console.log('Server Started 8000'));
+
+app.listen(process.env.port, () => console.log('Server Started 8000'));
